b4b67c70e82a42a6309b4ba67da93b15c090a19b
TristanP <tristanpere@googlemail.com>
2023-01-20 12:32:01 +0000

    operator buttons working with ac working. Next focus is back space button

--- main.js
+++ main.js
@@ -11,50 +11,90 @@ const leftBracket = document.querySelector(".keys__key--left-perenth")
 // keys__key--percent
 // keys__key--plus-minus
 // keys__key--divide
-// keys__key--plus
+const additionButton = document.querySelector(".keys__key--plus")
 // keys__key--minus
 // keys__key--point
 // keys__key--equal
+
 let displayArr = []
 let storageNumberArr = []
 let activeNumberArr = []
 let operatorArr = []
+let totalNumber = 0
 
 
 
-console.log(keys)
+// console.log(keys)
 
 //opperators have their own class
 
 numbers.forEach((number) =>{
     number.addEventListener("click", (event)=> {
+        // if (storageNumberArr<1) {
+
+        // }
         displayArr.push(event.target.innerText)
         activeNumberArr.push(event.target.innerText)
         display.innerText = displayArr.join("")
         // console.log(activeNumberArr)
         // console.log(displayArr)
         // console.log(operatorArr)
-        operatorArr = []
     })
 })
+// additionButton.addEventListener("click", ()=>{
+//     // const addition = (x,y) => {
+//     //     return x+y
+//     // }
 
+//     if (operatorArr[0]=="+"){}
+//         totalNumber = Number(storageNumberArr.join("")) + Number(activeNumberArr.join(""))
+//     } 
+//     console.log(storageNumberArr)
+//     console.log(totalNumberArr)
+// )
+
+// +,-,*,/
 operators.forEach((operator) =>{
     operator.addEventListener("click", (event)=>{
         console.log(operatorArr)
-        if (operatorArr.length<1){
+        if (operatorArr.length==0 & activeNumberArr>0){
             displayArr.push(event.target.innerText)
-            
+            storageNumberArr = [...activeNumberArr]
+            activeNumberArr = []
+            operatorArr[0] = event.target.innerText
             // console.log(displayArr)
-        } else {
+        } else if (operatorArr.length==0 & activeNumberArr==0){
+            // operatorArr[0] = event.target.innerText
+        } else if (operatorArr.length==1 & activeNumberArr==0){
             displayArr.pop()
             displayArr.push(event.target.innerText)
-            // console.log(displayArr)
+            operatorArr[0] = event.target.innerText
         }
-        operatorArr[0] = event.target.innerText
-
+        else { 
+            if (operatorArr[0]=="*"){
+                totalNumber = Number(storageNumberArr.join("")) * Number(activeNumberArr.join(""))
+            } else if (operatorArr[0]=="/"){
+                totalNumber = Number(storageNumberArr.join("")) / Number(activeNumberArr.join(""))
+            } else if (operatorArr[0]=="+"){
+                totalNumber = Number(storageNumberArr.join("")) + Number(activeNumberArr.join(""))
+            } else if (operatorArr[0]=="-"){
+                totalNumber = Number(storageNumberArr.join("")) - Number(activeNumberArr.join(""))
+            }
+            storageNumberArr = []
+            storageNumberArr = totalNumber.toString().split("")
+            activeNumberArr = []
+            operatorArr[0] = event.target.innerText
+            displayArr = []
+            displayArr = [...storageNumberArr]
+            displayArr.push(event.target.innerText)
+        }
+            // displayArr.push(event.target.innerText)
         display.innerText = displayArr.join("")
     })
 })
+
+
+
 
 // keys.forEach((key) =>{
 //     key.addEventListener("click", (event)=> {
@@ -66,7 +106,10 @@ operators.forEach((operator) =>{
 
 clearAll.addEventListener("click",()=>{
     displayArr = []
-    console.log(displayArr)
+    storageNumberArr = []
+    activeNumberArr = []
+    operatorArr = []
+    // console.log(displayArr)
     display.innerText = displayArr.join("")
 })
 clearLast.addEventListener("click",()=>{
@@ -74,7 +117,9 @@ clearLast.addEventListener("click",()=>{
         displayArr=[]
     } else {
         displayArr.pop()
+        activeNumberArr.pop()
     }
+    // if(operatorArr)
     display.innerText = displayArr.join("")
     console.log(displayArr)
 })
