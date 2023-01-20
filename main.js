6740f8582c98bd03e75c0d5a8484f5b003c64d8e
TristanP <tristanpere@googlemail.com>
2023-01-20 11:05:13 +0000

    Changed id for operators and numbers so display doesn't allow multiple operators next to each other.

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 const keys = document.querySelectorAll(".keys__key")
+const operators = document.querySelectorAll("#operator")
+const numbers = document.querySelectorAll("#number")
 const display = document.querySelector(".display")
 const clearAll = document.querySelector(".keys__key--all-clear")
 const clearLast = document.querySelector(".keys__key--backspace")
+const leftBracket = document.querySelector(".keys__key--left-perenth")
+// keys__key--right-perenth
+// keys__key--multiply
+// keys__key-- sqrt
+// keys__key--percent
+// keys__key--plus-minus
+// keys__key--divide
+// keys__key--plus
+// keys__key--minus
+// keys__key--point
+// keys__key--equal
 let displayArr = []
+let storageNumberArr = []
+let activeNumberArr = []
+let operatorArr = []
 
 
 
@@ -10,13 +26,44 @@ console.log(keys)
 
 //opperators have their own class
 
-keys.forEach((key,index) =>{
-    key.addEventListener("click", (event)=> {
+numbers.forEach((number) =>{
+    number.addEventListener("click", (event)=> {
         displayArr.push(event.target.innerText)
-        console.log(displayArr)
+        activeNumberArr.push(event.target.innerText)
+        display.innerText = displayArr.join("")
+        // console.log(activeNumberArr)
+        // console.log(displayArr)
+        // console.log(operatorArr)
+        operatorArr = []
+    })
+})
+
+operators.forEach((operator) =>{
+    operator.addEventListener("click", (event)=>{
+        console.log(operatorArr)
+        if (operatorArr.length<1){
+            displayArr.push(event.target.innerText)
+            
+            // console.log(displayArr)
+        } else {
+            displayArr.pop()
+            displayArr.push(event.target.innerText)
+            // console.log(displayArr)
+        }
+        operatorArr[0] = event.target.innerText
+
         display.innerText = displayArr.join("")
     })
 })
+
+// keys.forEach((key) =>{
+//     key.addEventListener("click", (event)=> {
+//         displayArr.push(event.target.innerText)
+//         console.log(displayArr)
+//         display.innerText = displayArr.join("")
+//     })
+// })
+
 clearAll.addEventListener("click",()=>{
     displayArr = []
     console.log(displayArr)
@@ -27,8 +74,11 @@ clearLast.addEventListener("click",()=>{
         displayArr=[]
     } else {
         displayArr.pop()
-        displayArr.pop()
     }
     display.innerText = displayArr.join("")
     console.log(displayArr)
 })
+
+// const addition = () => {
+
+// }
